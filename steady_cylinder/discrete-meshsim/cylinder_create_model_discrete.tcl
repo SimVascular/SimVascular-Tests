30042b4b80713db2da3807f57482b759a242f51c
nwilson <nwilson@osmsc.com>
2015-07-19 19:14:12 +0000

    reorganizing the steady cylinder example to try and reduce duplication in the tcl scripts

---
+++ steady_cylinder/discrete-meshsim/cylinder_create_model_discrete.tcl
@@ -0,0 +1,30 @@
+
+proc cylinder_create_model_Discrete {dstdir} {
+
+  # just copy the model for now
+
+  set fn cylinder.dsm
+
+  catch {repos_delete -obj cyl}
+  solid_readNative -file $fn -obj cyl
+
+  file copy $fn $dstdir
+  file copy $fn.facenames $dstdir
+
+  global gDiscreteModelFaceNames
+  global gDiscreteModelFaceNamesInfo
+  catch {unset gDiscreteModelFaceNames}
+  catch {unset gDiscreteModelFaceNamesInfo}
+  if [file exists $fn.facenames] {
+    puts "sourcing $fn.facenames"
+    source $fn.facenames
+    package require md5
+    set mymd5 [::md5::md5 -hex -file $fn]
+    if {$mymd5 != $gDiscreteModelFaceNamesInfo(model_file_md5)} {
+      return -code error "ERROR: dsm model ($fn) file doesn't match one used to generate facenames ($fn.facenames)!"
+    }
+  }
+
+  return [file join $dstdir $fn]
+
+}
---
+++ steady_cylinder/generic/steady_cylinder_create_bc_files_generic.tcl
@@ -0,0 +1,54 @@
+
+proc steady_cylinder_create_bc_files_generic {solidfn dstdir} {
+
+  #
+  #  Create an analytic Inflow Waveform and create a flow file.
+  #  Also calculate the FFT of the waveform for later.
+  #
+
+  puts "Generating sinusodial volumetric flow waveform."
+  set viscosity 0.004
+  set density 0.00106
+  set T 0.2
+  set Vbar 135
+  set radius 2
+  set omega [expr 2.0*[math_pi]/$T]
+
+  # calculate FFT terms
+  set pts {}
+  file mkdir [file join $dstdir flow-files]
+  set fp [open [file join $dstdir flow-files inflow.flow] "w"]
+  puts $fp "\#  Time (sec)   Flow (mm^3/sec)"
+  puts $fp "0   -1570.796327"
+  puts $fp "0.2 -1570.796327"
+  close $fp
+
+  puts "Write bct.dat and bct.vtp files."
+  global gBC
+  global guiABC
+  global gFilenames
+
+  # load in the solid used for meshing
+  set gFilenames(atdb_solid_file) [file join $dstdir $solidfn]
+  wormGUIloadSolidModel
+
+  # set params
+  set gBC(period) $T
+  set guiABC(viscosity) $viscosity
+  set guiABC(density) $density
+  set guiABC(type_of_profile) plug
+
+  set guiABC(mesh_face_file) ""
+  set guiABC(flow_rate_file) ""
+  set guiABC(bct_dat_file)   ""
+  set guiABC(bct_vtp_file)   ""
+  set guiABC(face_name) {$facename}
+  set guiABC(mesh_face_file) [file join $dstdir mesh-complete mesh-surfaces \$facename.vtp]
+  set guiABC(flow_rate_file) [file join $dstdir flow-files \$facename.flow]
+  set guiABC(bct_dat_file)   [file join $dstdir bct.dat.\$facename]
+  set guiABC(bct_vtp_file)   [file join $dstdir bct.vtp.\$facename]
+
+  # write files
+  wormGUIwriteMultipleFaces
+}
+
