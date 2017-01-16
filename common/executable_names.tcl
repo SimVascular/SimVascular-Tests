e20cb4597ddb6b7643104fe5f1d690b1649be159
osmsc <nwilson@osmsc.com>
2017-01-16 03:06:38 -0800

    BUG: fixed names of execs, removed references to adaptor

--- common/executable_names.tcl
+++ common/executable_names.tcl
@@ -1,13 +1,11 @@
 
 # central area to change name of executables
 
-set PRESOLVER     $gExternalPrograms(cvpresolver)
-set POSTSOLVER    $gExternalPrograms(cvpostsolver)
+set PRESOLVER     $gExternalPrograms(svpre)
+set POSTSOLVER    $gExternalPrograms(svpost)
 set MPIEXEC       $gExternalPrograms(mpiexec)
-set SOLVER        $gExternalPrograms(cvflowsolver)
-set FLOWSOLVER_CONFIG [file dirname $gExternalPrograms(cvflowsolver)] 
-set ADAPTOR       $gExternalPrograms(cvadaptor)
-set TETADAPTOR    $gExternalPrograms(cvtetadaptor)
+set SOLVER        $gExternalPrograms(svsolver-mpi)
+set FLOWSOLVER_CONFIG [file dirname $gExternalPrograms(svsolver-mpi)] 
 
 if {0 == 1} {
 
@@ -17,7 +15,6 @@ if {0 == 1} {
   set SOLVER        "$SVRELEASEDIR/flowsolver-bin.exe"
   set FLOWSOLVER_CONFIG [file dirname $SOLVER] 
   set POSTSOLVER    "$SVRELEASEDIR/postsolver-bin.exe"
-  set ADAPTOR       "$SVRELEASEDIR/adaptor-bin.exe"
 
 }
 
@@ -26,26 +23,7 @@ if {0 == 1} {
   set POSTSOLVER    /home/danschi/Development/svDevelopment/simvascular_reorg/Code/mypost
   set MPIEXEC       /usr/local/bin/mpiexec
   set SOLVER        /home/danschi/Development/svDevelopment/simvascular_reorg/Code/mysolver
-  set FLOWSOLVER_CONFIG [file dirname $gExternalPrograms(cvflowsolver)] 
-  set ADAPTOR       /home/danschi/Development/svDevelopment/simvascular_reorg/Code/myadaptor
-}
-
-if {0 == 1} {
-  set PRESOLVER     /Users/jmerkow/code/SimVascular/bin/simvascular-master/Bin/presolver
-  set POSTSOLVER    /Users/jmerkow/code/SimVascular/bin/simvascular-master/mypost
-  set MPIEXEC       /opt/local/bin/mpiexec
-  set SOLVER        /Users/jmerkow/code/SimVascular/bin/simvascular-master/mysolver
-  set FLOWSOLVER_CONFIG [file dirname $gExternalPrograms(cvflowsolver)] 
-  set ADAPTOR       /Users/jmerkow/code/SimVascular/bin/simvascular-master/myadaptor
-}
-
-if {0 == 1} {
-  set PRESOLVER     /home/jameson/Code/SimVascular/bin/simvascular-dev/Bin/presolver
-  set POSTSOLVER    /home/jameson/Code/SimVascular/bin/simvascular-dev/mypost
-  set MPIEXEC       /usr/bin/mpiexec
-  set SOLVER        /home/jameson/Code/SimVascular/bin/simvascular-dev/mysolver
-  set FLOWSOLVER_CONFIG [file dirname $gExternalPrograms(cvflowsolver)] 
-  set ADAPTOR       /home/jameson/Code/SimVascular/bin/simvascular-dev/myadaptor
+  set FLOWSOLVER_CONFIG [file dirname $gExternalPrograms(svsolver-mpi)] 
 }
 
 if {0 == 1} {
@@ -54,7 +32,7 @@ set PRESOLVER     "$MY_SIMVASCULAR_HOME/Code/Build/mypre"
 set POSTSOLVER    "$MY_SIMVASCULAR_HOME/Code/Build/mypost"
 set MPIEXEC       /opt/local/bin/mpiexec-mpich-gcc49
 set SOLVER        "$MY_SIMVASCULAR_HOME/Code/Build/mysolver"
-set FLOWSOLVER_CONFIG [file dirname $gExternalPrograms(cvflowsolver)] 
+set FLOWSOLVER_CONFIG [file dirname $gExternalPrograms(svsolver-mpi)] 
 }
 
 if {0 == 1} {
@@ -63,5 +41,5 @@ set PRESOLVER     "$MY_SIMVASCULAR_HOME/Code/Build/mypre"
 set POSTSOLVER    "$MY_SIMVASCULAR_HOME/Code/Build/mypost"
 set MPIEXEC       /usr/bin/mpiexec
 set SOLVER        "$MY_SIMVASCULAR_HOME/Code/Build/mysolver"
-set FLOWSOLVER_CONFIG [file dirname $gExternalPrograms(cvflowsolver)] 
+set FLOWSOLVER_CONFIG [file dirname $gExternalPrograms(svsolver-mpi)] 
 }
