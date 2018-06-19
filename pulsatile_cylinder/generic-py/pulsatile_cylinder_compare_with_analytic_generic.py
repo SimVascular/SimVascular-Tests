58820e3fdd1250e151bf6168b2e4cea587a4f479
Fanwei Kong <fanweikong@amir-mac.dyn.berkeley.edu>
2018-06-18 20:32:07 -0700

    DEV:added Python script to compare result with analytic solution

---
+++ pulsatile_cylinder/generic-py/pulsatile_cylinder_compare_with_analytic_generic.py
@@ -0,0 +1,173 @@
+# Copyright (c) Stanford University, The Regents of the University of
+#               California, and others.
+#
+# All Rights Reserved.
+#
+# See Copyright-SimVascular.txt for additional details.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject
+# to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+# OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#
+#  user defined list of res files to compare
+#
+import pulsatile_cylinder as pc
+import pulsatile_cylinder_generic2 as gen
+import math
+import pyMath
+import pyRepository
+import pyGeom
+import vtk
+import matplotlib.pyplot as plt
+
+timesteps = int(pc.timesteps)
+timesteps = 16
+tlist = [0.025, 0.075,0.125,0.175]
+toverTlist = [0.125,0.375,0.625,0.875]
+
+simstepnumlist = [int(timesteps*(1 + tlist[0]/0.2)) + 1,\
+                  int(timesteps*(1 + tlist[1]/0.2)) + 1,\
+                  int(timesteps*(1 + tlist[2]/0.2)) + 1,\
+                  int(timesteps*(1 + tlist[3]/0.2)) + 1]
+         
+fullrundir = gen.fullrundir
+resfn = fullrundir + '/cylinder_results.vtu'
+
+viscosity = 0.004
+density = 0.00106
+T = 0.2
+Vbar = 135
+radius = 2.0
+omega = 2.0*math.pi/T
+
+pts = []
+for i in range(0,256):
+    dt = T/255.0
+    t = float(i)*dt
+    Vmean = Vbar*(1.0+math.sin(2*math.pi*t/T))
+    area = math.pi*radius*radius
+    pts.append([t, -Vmean*area])
+    
+terms = pyMath.math_FFT(pts, 2,256)
+try:
+    pyRepository.repos_delete('outflow')
+except:
+    pass
+    
+import os
+import stat
+os.chmod(fullrundir+'/mesh-complete/mesh-surfaces', 0o777)
+pyRepository.repos_readXMLPolyData('outflow',fullrundir+'/mesh-complete/mesh-surfaces/outlet.vtp')
+numPts = pyGeom.geom_numPts('outflow')
+outflowObj = pyRepository.repos_exportToVtk('outflow')
+outflowScalars = outflowObj.GetPointData().GetScalars()
+
+print "Reading simulation results: " + resfn
+resReader = vtk.vtkXMLUnstructuredGridReader()
+resReader.SetFileName(resfn)
+resReader.Update()
+
+# now calculate flow rate for each time point
+countem = 0
+fig, axarr = plt.subplots(2, 2)
+fig.suptitle("Comparison between analytic results and simulation results", fontsize=16)
+for stepnum in simstepnumlist:
+    time = tlist[countem]
+    countem = countem+1
+    myVectors = vtk.vtkFloatArray()
+    myVectors.SetNumberOfComponents(3)
+    myVectors.Allocate(1000,1000)
+    
+    pointData = resReader.GetOutput().GetPointData()
+    nm = '00000'
+    nm = list(nm)
+    stpnum = list(str(stepnum))
+    nm[-len(stpnum):]=stpnum[-len(stpnum):]
+    nm = ''.join(nm)
+    objVectors = pointData.GetArray('velocity_'+nm)
+    
+    for i in range(0,int(numPts)):
+        node = outflowScalars.GetValue(i)
+        # nodes in vtk start at 0, so subtract 1 off
+        node = node-1
+        vec = objVectors.GetTuple3(node)
+        myVectors.InsertNextTuple3(vec[0],vec[1],vec[2])
+        
+    outflowObj.GetPointData().SetVectors(myVectors)
+    try:
+        pyRepository.repos_delete('outflowTmp')
+    except:
+        pass
+    pyRepository.repos_importVtkPd(outflowObj,'outflowTmp')
+    
+    fp = open(fullrundir+'/profiles_for_'+str(time),'w+')
+    fp.write("radius\tr/R\tanalytic\t")
+    fp.write("%d\t\n"% stepnum)
+    
+    analytic = []
+    result = []
+    xR = []
+    for j in range(-20,21):
+        r = float(j)/10.
+        xR.append(r/radius)
+        fp.write("%.4f\t%.4f\t"%(r,r/radius))
+        womersley = pyMath.math_computeWomersley(terms,time,viscosity,omega,density,radius,r)
+        try:
+            tmpvec = pyGeom.geom_interpolateVector('outflowTmp',[r,0.,0.])
+        except:
+            tmpvec = [0]
+            pass
+        
+        analytic.append(womersley)
+        if len(tmpvec)==3:
+            fp.write("%.4f\t%.4f\t\n"%(womersley,tmpvec[2]))
+            result.append(tmpvec[2])
+        else:
+            fp.write("%.4f\t\t\n"%(womersley))
+            
+    fp.close()
+    if countem==1:
+        axarr[0, 0].plot(xR, result/10,'r')
+        axarr[0, 0].plot(xR, analytic/10,'g')
+        axarr[0, 0].set_title('Polydata_tetgen t/T='+str(time))
+    elif countem==2:
+        axarr[0, 1].plot(xR, result/10,'r')
+        axarr[0, 1].plot(xR, analytic/10,'g')
+        axarr[0, 1].set_title('Polydata_tetgen t/T='+str(time))
+    elif countem==3:
+        axarr[1, 0].plot(xR, result/10,'r')
+        axarr[1, 0].plot(xR, analytic/10,'g')
+        axarr[1, 0].set_title('Polydata_tetgen t/T='+str(time))
+    elif countem==4:
+        axarr[1, 1].plot(xR, result/10,'r')
+        axarr[1, 1].plot(xR, analytic/10,'g')
+        axarr[1, 1].set_title('Polydata_tetgen t/T='+str(time))
+
+    
+# Tight layout often produces nice results
+# but requires the title to be spaced accordingly
+fig.tight_layout()
+fig.subplots_adjust(top=0.88)
+
+plt.show()
+
