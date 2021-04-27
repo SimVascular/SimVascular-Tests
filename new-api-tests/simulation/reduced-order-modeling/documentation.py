60a8b98e52fcc445aac80fca5e8e3eaac8ee59d2
Dave Parker <davep@stanford.edu>
2021-04-26 20:24:50 -0700

    Rename 1d simulation to reduced order and change the module name, etc.

---
+++ new-api-tests/simulation/reduced-order-modeling/documentation.py
@@ -0,0 +1,10 @@
+'''Test the 'simulation' 1d simulation class documentation.
+'''
+import sv
+
+#help(sv.simulation.ReducedOrderModeling)
+
+#help(sv.simulation.ReducedOrderModeling)
+
+help(sv.simulation.ROMParameters.WallProperties)
+
