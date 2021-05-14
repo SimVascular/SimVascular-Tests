32ec9d2595113523fd6fe08c8ca6f7d1bfdf3882
Dave Parker <davep@stanford.edu>
2021-05-13 21:56:18 -0700

    Add remeshing the capped surface and generating a fe volume mesh.

--- new-api-tests/applications/create-surface-caps-from-centerlines/run.sh
+++ new-api-tests/applications/create-surface-caps-from-centerlines/run.sh
@@ -2,8 +2,6 @@
 
 svp='/Users/parkerda/software/ktbolt/SimVascular/build/SimVascular-build/sv --python -- '
 
-file=H-009_M6__28_surfacemesh.vtk 
-
 file=H-015_M6__17_surfacemesh.vtk 
 
 file=H-030_J0__27_surfacemesh.vtk
@@ -14,5 +12,8 @@ file=H-019_J0__11_surfacemesh.vtk
 
 file=H-053_J0__06_surfacemesh.vtk 
 
-$svp create_surface_caps.py --surface-file=./data/${file} --clip-distance=0.2  --clip-width-scale=1.1
+file=H-009_M6__28_surfacemesh.vtk 
+
+
+$svp create_surface_caps.py --surface-file=./data/${file} --clip-distance=0.2  --clip-width-scale=1.0
 
