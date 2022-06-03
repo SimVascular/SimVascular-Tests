8e29f8f0afe5da985027e25a73836be9efc389b6
Dave Parker <davep@stanford.edu>
2022-06-02 17:16:38 -0700

    Add a couple of tests.

--- new-api-tests/dmg/add-mesh.py
+++ new-api-tests/dmg/add-mesh.py
@@ -20,18 +20,22 @@ file_name = home + "/SimVascular/DemoProject/Meshes/demo.vtu"
 reader = vtk.vtkXMLUnstructuredGridReader()
 reader.SetFileName(file_name)
 reader.Update()
-mesh = reader.GetOutput()
+volume_mesh = reader.GetOutput()
 print("Mesh: ");
 print("  Number of nodes: {0:d}".format(mesh.GetNumberOfPoints()))
 print("  Number of elements: {0:d}".format(mesh.GetNumberOfCells()))
+
+## Read a model
+modeler = sv.modeling.Modeler(sv.modeling.Kernel.POLYDATA)
+file_name = str(data_path / 'DemoProject' / 'Models' / 'demo.vtp')
+model = modeler.read(file_name)
+model_surface = model.get_polydata()
 
 ## Add a vtk unstructure mesh object under the SV Data Manager 'Meshes' node
 #  as a new  node named 'new_demo'.
 #
 try:
-    sv.dmg.add_mesh(name="new_demo", mesh=mesh, model="demo")
-    #sv.dmg.add_mesh(name="new_demo", mesh=mesh, model="demom")
-    #sv.dmg.add_mesh("new_demo", reader)
+    sv.dmg.add_mesh(name="new_demo", volume=volume_mesh, surface=model_surface, model="demo")
 except Exception as inst:
     print("Exception")
     print(inst)
